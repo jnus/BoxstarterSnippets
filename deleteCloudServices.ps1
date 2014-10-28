80b2e6ad6c30d318b590f2c4161caeee77e75d6e
Jasper Nygaard <jasper@sovs.net>
2014-10-28 08:33:04 +0100

    Delete all cloud services provisioned by these examples

---
+++ deleteCloudServices.ps1
@@ -0,0 +1,12 @@
+$services = Get-AzureService
+
+$deleting = Get-AzureService | Where-Object {$_.Label -like "BoxStarterTestService*"}
+
+Write-Host "Deleting cloud services"
+$deleting | Format-Table
+
+$deleting | ForEach-Object {
+    Remove-AzureService -ServiceName $_.ServiceName -DeleteAll
+}
+
+
